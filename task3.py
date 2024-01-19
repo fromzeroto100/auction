f389213591692bda2fc847bdc8e133aeab2628aa
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-19 11:38:35 +0100

    new1

--- task3.py
+++ task3.py
@@ -4,3 +4,14 @@
 # }
 
 
+# cars = {
+#     "rel cars": ["Toyota", "Honda", "Audi"],
+#     "Sports cars": ["BMW", "Lamborgini", "Bugatti"]
+# }
+
+cars = {
+    "Rel cars": {"Toyota": ["Land cruiser", "Hillux"]}, 
+    
+}
+
+print(cars)
